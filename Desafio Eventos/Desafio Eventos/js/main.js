f920a58401a8a9bfc2201211fe4ce79155b47187
CurthbertAllgood <101150271+CurthbertAllgood@users.noreply.github.com>
2022-07-21 16:45:04 -0300

    actualizacion desafio eventos

---
+++ Desafio Eventos/Desafio Eventos/js/main.js
@@ -0,0 +1,217 @@
+
+
+const pedidototal=[];
+
+
+
+//Boton Comida
+let opcion= document.getElementById("Opciones");
+
+
+opcion.onclick= () =>{RenderizarComida();
+}
+//Boton Bebidas
+let opcion1= document.getElementById("Opciones2");
+
+
+opcion1.onclick= () =>{RenderizarBebida();
+}
+//Boton Postre
+let opcion3= document.getElementById("Opciones3");
+
+
+opcion3.onclick= () =>{RenderizarPostre();
+}
+
+
+
+
+//Renderiza el menu solo comida
+function RenderizarComida(){
+
+    const tienda = document.getElementById('tienda');
+    
+    tienda.innerHTML = '';
+    let value=BBDD.filter(function(tipo){
+        return (tipo.codigo==='C');
+    });
+
+        value.forEach((p)=>{
+            
+            let productoHTML = `
+    
+            <div class="col-12 col-md-4 mb-5 d-flex justify-content-center">
+            <div class="card text-dark" style="width: 18rem;">
+            <img class="card-img-top" src="${p.img}" alt="Imagen Comida">
+                <div class="card-body">
+                    <h5 class="card-title">${p.nombre}</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p>${p.precio}$</p>
+                    <button class="btn btn-primary" onclick="SumarPedido(${p.id})">Añadir al pedido</button>
+                </div>
+            </div>
+            </div>
+            `
+            
+            tienda.innerHTML += productoHTML;
+        });
+
+}
+
+//Renderiza el menu solo bebidas
+function RenderizarBebida(){
+
+    
+    const tienda = document.getElementById('tienda');
+    
+    tienda.innerHTML = '';
+
+    let value=BBDD.filter(function(tipo){
+        return (tipo.codigo==='B');
+    });
+
+
+        value.forEach((p)=>{
+            
+            let productoHTML = `
+    
+            <div class="col-12 col-md-4 mb-5 d-flex justify-content-center">
+            <div class="card text-dark" style="width: 18rem;">
+            <img class="card-img-top" src="${p.img}" alt="Imagen Bebida">
+                <div class="card-body">
+                    <h5 class="card-title">${p.nombre}</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p>${p.precio}$</p>
+                    <button class="btn btn-primary" onclick="SumarPedido(${p.id})">Añadir al pedido</button>
+                </div>
+            </div>
+            </div>
+            `
+            
+            tienda.innerHTML += productoHTML;
+        });
+}
+
+//Renderiza el menu solo postre
+function RenderizarPostre(){
+
+
+    const tienda = document.getElementById('tienda');
+    
+    tienda.innerHTML = '';
+
+    let value=BBDD.filter(function(tipo){
+        return (tipo.codigo==='P');
+    });
+
+        value.forEach((p)=>{
+            
+            let productoHTML = `
+    
+            <div class="col-12 col-md-4 mb-5 d-flex justify-content-center">
+            <div class="card text-dark" style="width: 18rem;">
+            <img class="card-img-top" src="${p.img}" alt="Imagen postre">
+                <div class="card-body">
+                    <h5 class="card-title">${p.nombre}</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p>${p.precio}$</p>
+                    <button class="btn btn-primary" onclick="SumarPedido(${p.id})">Añadir al pedido</button>
+                </div>
+            </div>
+            </div>
+            `
+            
+            tienda.innerHTML += productoHTML;
+        });
+}
+//Suma productos al pedido de la mesa
+
+function RenderizaPedido(){
+    let pedido = document.querySelector('#pedido');    
+    let pedidoHtml =' ';
+
+        pedidototal.forEach((p, id)=>{
+            
+            pedidoHtml += `
+    
+            <div class="col-12 col-md-4 mb-5 d-flex justify-content-center">
+            <div class="card text-dark" style="width: 18rem;">
+                <img class="card-img-top" src="${p.img}" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">${p.nombre}</h5>
+                    <p>${p.precio}$</p>
+                    <p>Cantidad: ${p.cantidad}</p>
+                    <button class="btn btn-danger" onclick="RestarPedido(${id})">Eliminar</button>
+                </div>
+            </div>
+            </div>
+            `
+            
+            
+    })
+    pedido.innerHTML = pedidoHtml;
+    GuardarPedido();
+}
+
+//Suma y resta del pedido
+
+
+function SumarPedido(id){
+    
+    let pedidos = BBDD.find(pedidos => pedidos.id === id);
+    let ListaPedidos = pedidototal.find(pedidos => pedidos.id === id);
+        if(ListaPedidos){
+            ListaPedidos.cantidad++;
+        }
+        else {
+        pedidos.cantidad = 1;
+        pedidototal.push(pedidos);
+        console.log(pedidototal);
+        }
+    RenderizaPedido();
+    CalcularPrecioFinal();
+
+}
+
+//Guarda el pedido en el storage
+function GuardarPedido(){
+    for(pedido of pedidototal){
+        GuardarStorage(pedido.nombre , JSON.stringify(pedido))
+    }
+};
+
+const GuardarStorage= (clave, valor) =>{
+    localStorage.setItem(clave,valor);
+}
+
+//borra el storage 
+
+function BorrarStorage(){
+    localStorage.clear();
+}
+
+//Elimina elemento del pedido y actualiza el storage
+
+function RestarPedido(id){
+    BorrarStorage();
+    pedidototal[id].cantidad--;
+        if(pedidototal[id].cantidad===0){
+        pedidototal.splice(id,1);
+    }
+    GuardarPedido();
+    RenderizaPedido();
+    CalcularPrecioFinal();
+
+}
+//Calcula precio final
+
+function CalcularPrecioFinal(){
+    
+    let acu=0;
+    pedidototal.forEach((p)=>{
+        acu += p.precio * p.cantidad;
+    });
+    console.log("Precio final $"+acu);
+    const t = document.getElementById('total');
+    t.innerHTML = `<h5>El total del pedido es: $${acu}</h5>`;
+}
---
+++ Desafio Eventos/Desafio Eventos/js/stock.js
@@ -0,0 +1,53 @@
+const BBDD = [
+    {
+        "id": 1,
+        "codigo":"C",
+        "nombre": "Pizza",
+        "img": "/assets/img/pizza.png",
+        "precio": 150,
+        "cantidad":1
+    },
+    {
+        "id": 2,
+        "codigo":"C",
+        "nombre": "sushi",
+        "img": "/assets/img/sushi.png",
+        "precio": 200,
+        "cantidad":1
+    },
+
+    {
+        "id": 3,
+        "codigo":"B",
+        "nombre": "Caipi",
+        "img": "/assets/img/Caipi.png",
+        "precio": 50,
+        "cantidad":1
+    },
+    {
+        "id": 4,
+        "codigo":"B",
+        "nombre": "Cuba Libre",
+        "img": "/assets/img/CubaLibre.png",
+        "precio": 60,
+        "cantidad":1
+    },
+
+    {
+        "id": 5,
+        "codigo":"P",
+        "nombre": "Helado",
+        "img": "/assets/img/Helado.png",
+        "precio": 30,
+        "cantidad":1
+    },
+
+    {
+        "id": 6,
+        "codigo":"P",
+        "nombre": "flan",
+        "img": "/assets/img/flan.png",
+        "precio": 35,
+        "cantidad":1
+    },
+]
